4a7fa249d168cc6f7e4aa92be55e66394362daf2
theanarkh <theratliter@gmail.com>
2022-06-22 22:38:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const addon = require.resolve('./build/Release/addon.node');
-// this makes addon not be unloaded by dlclose
+// this makes addon not be unloaded
 // require(addon);
 const { Worker } = require('worker_threads');
 new Worker(`require('${addon}').start();`, {eval: true});
